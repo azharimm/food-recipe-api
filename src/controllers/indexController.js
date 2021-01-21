02a9fcb94235c873e1edacd779645abb6a8ba31b
Azhari Marzan <azhari.marzan@gmail.com>
2021-01-21 14:34:06 +0700

    ^ exception error handling

--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -12,111 +12,128 @@ exports.index = (req, res) => {
 };
 
 exports.new = async (req, res) => {
-    const htmlResult = await request.get(`${process.env.BASE_URL}`);
-    const $ = await cheerio.load(htmlResult);
-    const results = [];
-    $(".post-blocks-row")
-        .children("div")
-        .each((index, el) => {
-            let selector = $(el).find(".post-info");
-            const id = $(el)
-                .find("a")
-                .attr("href")
-                .replace(`${process.env.BASE_URL}/resep/`, "");
-            const title = selector.children(".title").text().trim();
-            const time = selector.find(".time").text().trim();
-            const servings = selector.find(".servings").text().trim();
-            const difficulty = selector.find(".difficulty").text().trim();
-
-            const result = {
-                id: id.substring(0, id.length - 1),
-                title,
-                time,
-                servings,
-                difficulty,
-            };
-
-            if (time && servings && difficulty) {
-                results.push(result);
-            }
-        });
-    return json(res, results);
+    try {
+        const htmlResult = await request.get(`${process.env.BASE_URL}`);
+        const $ = await cheerio.load(htmlResult);
+        const results = [];
+        $(".post-blocks-row")
+            .children("div")
+            .each((index, el) => {
+                let selector = $(el).find(".post-info");
+                const id = $(el)
+                    .find("a")
+                    .attr("href")
+                    .replace(`${process.env.BASE_URL}/resep/`, "");
+                const title = selector.children(".title").text().trim();
+                const time = selector.find(".time").text().trim();
+                const servings = selector.find(".servings").text().trim();
+                const difficulty = selector.find(".difficulty").text().trim();
+    
+                const result = {
+                    id: id.substring(0, id.length - 1),
+                    title,
+                    time,
+                    servings,
+                    difficulty,
+                };
+    
+                if (time && servings && difficulty) {
+                    results.push(result);
+                }
+            });
+        return json(res, results);
+        
+    } catch (error) {
+        return errorJson(res, error);
+    }
 };
 
 exports.categories = async (req, res) => {
-    const htmlResult = await request.get(`${process.env.BASE_URL}`);
-    const $ = await cheerio.load(htmlResult);
-    const categories = [];
-    $(".category-col").each((index, el) => {
-        const categoryId = $(el)
-            .find("a")
-            .attr("href")
-            .replace(`${process.env.BASE_URL}/resep-masakan/`, "");
-        const category = $(el).find("span").text();
-        categories.push({
-            categoryId: categoryId.substring(0, categoryId.length - 1),
-            category,
+    try {
+        const htmlResult = await request.get(`${process.env.BASE_URL}`);
+        const $ = await cheerio.load(htmlResult);
+        const categories = [];
+        $(".category-col").each((index, el) => {
+            const categoryId = $(el)
+                .find("a")
+                .attr("href")
+                .replace(`${process.env.BASE_URL}/resep-masakan/`, "");
+            const category = $(el).find("span").text();
+            categories.push({
+                categoryId: categoryId.substring(0, categoryId.length - 1),
+                category,
+            });
         });
-    });
-    return json(res, categories);
+        return json(res, categories);
+    } catch (error) {
+        return errorJson(res, error);        
+    }
 };
 
 exports.showCategory = async (req, res) => {
-    categoryId = req.params.categoryId;
-    const htmlResult = await request.get(`${process.env.BASE_URL}/resep-masakan/${categoryId}`);
-    const $ = await cheerio.load(htmlResult);
-    const results = [];
-    $(".post-blocks-row")
-        .children("div")
-        .each((index, el) => {
-            let selector = $(el).find(".post-info");
-            const id = $(el)
-                .find("a")
-                .attr("href")
-                .replace(`${process.env.BASE_URL}/resep/`, "");
-            const title = selector.children(".title").text().trim();
-            const time = selector.find(".time").text().trim();
-            const servings = selector.find(".servings").text().trim();
-            const difficulty = selector.find(".difficulty").text().trim();
+    try {
+        categoryId = req.params.categoryId;
+        const htmlResult = await request.get(`${process.env.BASE_URL}/resep-masakan/${categoryId}`);
+        const $ = await cheerio.load(htmlResult);
+        const results = [];
+        $(".post-blocks-row")
+            .children("div")
+            .each((index, el) => {
+                let selector = $(el).find(".post-info");
+                const id = $(el)
+                    .find("a")
+                    .attr("href")
+                    .replace(`${process.env.BASE_URL}/resep/`, "");
+                const title = selector.children(".title").text().trim();
+                const time = selector.find(".time").text().trim();
+                const servings = selector.find(".servings").text().trim();
+                const difficulty = selector.find(".difficulty").text().trim();
+    
+                const result = {
+                    id: id.substring(0, id.length - 1),
+                    title,
+                    time,
+                    servings,
+                    difficulty,
+                };
+    
+                if (time && servings && difficulty) {
+                    results.push(result);
+                }
+            });
+        return json(res, results);
+    } catch (error) {
+        return errorJson(res, error);        
+    }
+};
 
+exports.search = async (req, res) => {
+    try {
+        q = req.query.q;
+        if(!q) {
+            return errorJson(res, "Please provide query search!");
+        }
+        const htmlResult = await request.get(`${process.env.BASE_URL}/?s=${q}`);
+        const $ = await cheerio.load(htmlResult);
+        const results = [];
+        $(".post-col").each((index, el) => {
+            const title = $(el).find(".title").text().trim();
+            const time = $(el).find(".time").text().trim();
+            const servings = $(el).find(".servings").text().trim();
+            const difficulty = $(el).find(".difficulty").text().trim();
             const result = {
-                id: id.substring(0, id.length - 1),
                 title,
                 time,
                 servings,
-                difficulty,
-            };
-
+                difficulty
+            }
             if (time && servings && difficulty) {
                 results.push(result);
             }
         });
-    return json(res, results);
-};
-
-exports.search = async (req, res) => {
-    q = req.query.q;
-    if(!q) {
-        return errorJson(res, "Please provide query search!");
+    
+        return json(res, results);
+    } catch (error) {
+        return errorJson(res, error);        
     }
-    const htmlResult = await request.get(`${process.env.BASE_URL}/?s=${q}`);
-    const $ = await cheerio.load(htmlResult);
-    const results = [];
-    $(".post-col").each((index, el) => {
-        const title = $(el).find(".title").text().trim();
-        const time = $(el).find(".time").text().trim();
-        const servings = $(el).find(".servings").text().trim();
-        const difficulty = $(el).find(".difficulty").text().trim();
-        const result = {
-            title,
-            time,
-            servings,
-            difficulty
-        }
-        if (time && servings && difficulty) {
-            results.push(result);
-        }
-    });
-
-    return json(res, results);
 }
